f199bf43c6c89986f6a3f1f6cd4a16739d02e4c3
Jamshaid Nasar <jamshaidnasar@proton.me>
2026-02-03 17:31:23 +0100

    feat: enhance greeting and add age demonstration
    - Update name variable to full name "Jamshaid Nasar"
    - Change greeting message to "Welcome to rust {name}."
    - Introduce mutable age variable with initial value 16
    - Add statement to display initial age
    - Update age variable to 17
    - Add statement to display updated age

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     println!("Hello, world!");
 
-    let name = "Jamshaid";
-    println!("Hello, {}", name);
+    let name = "Jamshaid Nasar";
+    println!("Welcome to rust {name}.");
+
+    let mut age = 16;
+    println!("I was {age} years old.");
+
+    age = 17;
+    println!("I am {age} years old now.");
 }
